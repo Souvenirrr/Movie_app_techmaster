4a4d60442c258e9d1a9351d39db99a372293c8d2
Souvenirrr <tungtran130@gmail.com>
2019-12-08 13:44:53 +0700

    update router

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:movie_app/src/ui/booking_page.dart';
 import 'package:movie_app/src/ui/home_page.dart';
+import 'package:movie_app/src/ui/login.dart';
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      //home: HomePage(),
+      initialRoute: '/',
+      routes: {
+        '/' : (BuildContext context) => HomePage(),
+        '/login' : (BuildContext context) => Login(),
+      },
     );
   }
 }
