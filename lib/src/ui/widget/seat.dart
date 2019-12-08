5922a7af2ca426a25c83a75df043633b346c3d85
Souvenirrr <tungtran130@gmail.com>
2019-12-08 22:17:54 +0700

    update caculate money

--- lib/src/ui/widget/seat.dart
+++ lib/src/ui/widget/seat.dart
@@ -3,6 +3,8 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:movie_app/src/model/seatmodel.dart';
 import 'package:http/http.dart' as http;
+import 'package:provider/provider.dart';
+import 'package:movie_app/src/ui/seat_page.dart';
 
 class Seat extends StatefulWidget {
   String name_seat;
@@ -53,47 +55,47 @@ class _SeatState extends State<Seat> {
       color = Colors.red;
     } else if (check) {
       color = Colors.blue;
-      //widget.count++;
-      //print("tang: " + widget.count.toString());
     } else {
       color = Colors.white;
-      //widget.count--;
-      //print(widget.count.toString());
     }
   }
 
   @override
   Widget build(BuildContext context) {
+    Counter counter = Provider.of<Counter>(context);
     return seatModel == null
         ? Center(
-            child: CircularProgressIndicator(),
-          )
+      child: CircularProgressIndicator(),
+    )
         : Column(
-          children: <Widget>[
-            Container(
-                //color: Colors.blue,
-                //alignment: Alignment.topCenter,
-                margin: EdgeInsets.only(top: 20.0),
-                padding: EdgeInsets.all(5.0),
-                decoration: BoxDecoration(
-                  color: color,
-                  border: Border.all(color: Colors.black),
-                ),
-                child: InkWell(
-                  onTap: () {
-                    setState(() {
-                      check = !check;
-                      //color = Colors.blue;
-                      _setColor();
-                      _setCount();
-                      //print(_setColor());
-                      //print("check 1 " + check.toString());
-                    });
-                  },
-                  child: Text(widget.name_seat),
-                ),
-              ),
-          ],
-        );
+      children: <Widget>[
+        Container(
+          //color: Colors.blue,
+          //alignment: Alignment.topCenter,
+          margin: EdgeInsets.only(top: 20.0),
+          padding: EdgeInsets.all(5.0),
+          decoration: BoxDecoration(
+            color: color,
+            border: Border.all(color: Colors.black),
+          ),
+          child: InkWell(
+            onTap: () {
+              setState(() {
+                if (check) {
+                  counter.decrement();
+                  color = Colors.white;
+                } else {
+                  counter.increment();
+                  color = Colors.blue;
+                }
+                check = !check;
+                print(counter.count);
+              });
+            },
+            child: Text(widget.name_seat),
+          ),
+        ),
+      ],
+    );
   }
 }
