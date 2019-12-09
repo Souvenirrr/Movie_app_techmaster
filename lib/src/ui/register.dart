aa50b30659d2a1b0262bae5fdf67b7fbdae6ebd6
Souvenirrr <tungtran130@gmail.com>
2019-12-10 03:33:17 +0700

    check validator and post data to server

--- lib/src/ui/register.dart
+++ lib/src/ui/register.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:movie_app/src/model/register_model.dart';
 import 'package:validators/validators.dart' as validator;
+import 'package:http/http.dart' as http;
 
 class Register extends StatefulWidget {
   @override
@@ -9,9 +13,34 @@ class Register extends StatefulWidget {
 class _RegisterState extends State<Register> {
   final formRegis = GlobalKey<FormState>();
   var passKey = GlobalKey<FormFieldState>();
-  String phone;
+  String username;
   String password;
   String repassword;
+  String fullname;
+  String birthday;
+  String email;
+  String phone;
+  String city;
+  RegisterModel register;
+
+  Future<void> postRegister() async {
+    final response = await http.post(
+      'https://dgvapi.herokuapp.com/user/login',
+      body: jsonEncode({
+        "password": "$password",
+        "userBirthday": "$birthday",
+        "userCity": "$city",
+        "userEmail": "$email",
+        "userFullname": "$fullname",
+        "userPhone": "$phone",
+        "username": "$username"
+      }),
+      headers: {"Content-Type": "application/json"},
+    );
+    setState(() {
+      register = RegisterModel.fromJson(jsonDecode(response.body));
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,25 +58,25 @@ class _RegisterState extends State<Register> {
                 Container(
                   margin: EdgeInsets.only(top: 20, bottom: 10),
                   child: Image.asset('beta.png',
-                      width:MediaQuery.of(context).size.width/3),
+                      width: MediaQuery.of(context).size.width / 3),
                 ),
                 Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: TextFormField(
                     decoration: InputDecoration(
-                      hintText: "Phone",
+                      hintText: "Username",
                       border: InputBorder.none,
                       fillColor: Colors.grey[300],
                       filled: true,
                     ),
                     validator: (String value) {
-                      if (value.length  != 10 || !validator.isNumeric(value)) {
-                        return "Phone khong hop le";
+                      if (value.length == 0) {
+                        return "Username khong duoc de trong";
                       }
                       return null;
                     },
                     onSaved: (String value) {
-                      phone = value;
+                      username = value;
                     },
                   ),
                 ),
@@ -82,11 +111,11 @@ class _RegisterState extends State<Register> {
                       fillColor: Colors.grey[300],
                       filled: true,
                     ),
-                    obscureText: false,
+                    obscureText: true,
                     validator: (String value) {
                       if (value.isEmpty) {
                         return "Password khong duoc de trong";
-                      }else if(value != passKey.currentState.value){
+                      } else if (value != passKey.currentState.value) {
                         return "Password khong trung nhau";
                       }
                       return null;
@@ -96,21 +125,166 @@ class _RegisterState extends State<Register> {
                     },
                   ),
                 ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Fullname",
+                      border: InputBorder.none,
+                      fillColor: Colors.grey[300],
+                      filled: true,
+                    ),
+                    obscureText: false,
+                    validator: (String value) {
+                      if (value.isEmpty) {
+                        return "Fullname khong duoc de trong";
+                      }
+                      return null;
+                    },
+                    onSaved: (String value) {
+                      fullname = value;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Birthday(yy-mm-dd)",
+                      border: InputBorder.none,
+                      fillColor: Colors.grey[300],
+                      filled: true,
+                    ),
+                    obscureText: false,
+                    validator: (String value) {
+                      if (value.isEmpty) {
+                        return "Birthday khong duoc de trong";
+                      } else if (!validator.isDate(value)) {
+                        return "Birthday chua dung dinh dang";
+                      }
+                      return null;
+                    },
+                    onSaved: (String value) {
+                      birthday = value;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Email",
+                      border: InputBorder.none,
+                      fillColor: Colors.grey[300],
+                      filled: true,
+                    ),
+                    obscureText: false,
+                    validator: (String value) {
+                      if (value.isEmpty) {
+                        return "Email khong duoc de trong";
+                      } else if (!validator.isEmail(value)) {
+                        return "Email khong dung dinh dang";
+                      }
+                      return null;
+                    },
+                    onSaved: (String value) {
+                      email = value;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Phone",
+                      border: InputBorder.none,
+                      fillColor: Colors.grey[300],
+                      filled: true,
+                    ),
+                    obscureText: false,
+                    validator: (String value) {
+                      if (value.isEmpty) {
+                        return "Phone khong duoc de trong";
+                      } else if (!validator.isNumeric(value) ||
+                          value.length != 10) {
+                        return "Phone khong dung dinh dang";
+                      }
+                      return null;
+                    },
+                    onSaved: (String value) {
+                      phone = value;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "City",
+                      border: InputBorder.none,
+                      fillColor: Colors.grey[300],
+                      filled: true,
+                    ),
+                    obscureText: false,
+                    validator: (String value) {
+                      if (value.isEmpty) {
+                        return "City khong duoc de trong";
+                      }
+                      return null;
+                    },
+                    onSaved: (String value) {
+                      city = value;
+                    },
+                  ),
+                ),
                 RaisedButton(
                   color: Colors.blue,
-                  onPressed: () {
+                  onPressed: () async {
                     if (formRegis.currentState.validate()) {
                       formRegis.currentState.save();
-                      print("Email la: " + phone.toString());
-                      print("Password la: " + password.toString());
-                      //_createPost();
-                      //print("Dang nhap");
+                      await postRegister();
+                      print("success");
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            title: Text("Message"),
+                            content: Text("Dang ky thanh cong"),
+                            actions: <Widget>[
+                              FlatButton(
+                                child: Text("OK"),
+                                onPressed: () {
+                                  Navigator.pushNamed(context, '/');
+                                },
+                              )
+                            ],
+                          );
+                        },
+                      );
+                    } else {
+                      print("fail");
+                      showDialog(
+                          context: context,
+                          builder: (context) {
+                            return AlertDialog(
+                              title: Text("Message"),
+                              content: Text("Dang ky khong thanh cong"),
+                              actions: <Widget>[
+                                FlatButton(
+                                  child: Text("Close"),
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
+                                )
+                              ],
+                            );
+                          });
                     }
+                    print("out");
                   },
                   child: Text(
                     "Registration",
-                    style: TextStyle(
-                        fontSize: 18, color: Colors.white),
+                    style: TextStyle(fontSize: 18, color: Colors.white),
                   ),
                 ),
               ],
