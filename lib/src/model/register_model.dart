40a8eca502658155bea5b9f27aba5e6e2aa56ef5
Souvenirrr <tungtran130@gmail.com>
2019-12-10 03:11:47 +0700

    update model

---
+++ lib/src/model/register_model.dart
@@ -0,0 +1,21 @@
+class RegisterModel {
+  int status;
+  String msg;
+  int data;
+
+  RegisterModel({this.status, this.msg, this.data});
+
+  RegisterModel.fromJson(Map<String, dynamic> json) {
+    status = json['status'];
+    msg = json['msg'];
+    data = json['data'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['status'] = this.status;
+    data['msg'] = this.msg;
+    data['data'] = this.data;
+    return data;
+  }
+}
--- lib/src/model/seatmodel.dart
+++ lib/src/model/seatmodel.dart
@@ -1,24 +1,17 @@
 class SeatModel {
   String status;
   String msg;
-  Movie movie;
-  Schedule schedule;
-  List<SeatRows> seatRows;
+  List<Data> data;
 
-  SeatModel(
-      {this.status, this.msg, this.movie, this.schedule, this.seatRows});
+  SeatModel({this.status, this.msg, this.data});
 
   SeatModel.fromJson(Map<String, dynamic> json) {
     status = json['status'];
     msg = json['msg'];
-    movie = json['movie'] != null ? new Movie.fromJson(json['movie']) : null;
-    schedule = json['schedule'] != null
-        ? new Schedule.fromJson(json['schedule'])
-        : null;
-    if (json['seat_rows'] != null) {
-      seatRows = new List<SeatRows>();
-      json['seat_rows'].forEach((v) {
-        seatRows.add(new SeatRows.fromJson(v));
+    if (json['data'] != null) {
+      data = new List<Data>();
+      json['data'].forEach((v) {
+        data.add(new Data.fromJson(v));
       });
     }
   }
@@ -27,74 +20,20 @@ class SeatModel {
     final Map<String, dynamic> data = new Map<String, dynamic>();
     data['status'] = this.status;
     data['msg'] = this.msg;
-    if (this.movie != null) {
-      data['movie'] = this.movie.toJson();
-    }
-    if (this.schedule != null) {
-      data['schedule'] = this.schedule.toJson();
-    }
-    if (this.seatRows != null) {
-      data['seat_rows'] = this.seatRows.map((v) => v.toJson()).toList();
+    if (this.data != null) {
+      data['data'] = this.data.map((v) => v.toJson()).toList();
     }
     return data;
   }
 }
 
-class Movie {
-  String movieId;
-  String movieName;
-
-  Movie({this.movieId, this.movieName});
-
-  Movie.fromJson(Map<String, dynamic> json) {
-    movieId = json['movie_id'];
-    movieName = json['movie_name'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['movie_id'] = this.movieId;
-    data['movie_name'] = this.movieName;
-    return data;
-  }
-}
-
-class Schedule {
-  String scheduleId;
-  String scheduleDate;
-  String scheduleStart;
-  String scheduleEnd;
-
-  Schedule(
-      {this.scheduleId,
-        this.scheduleDate,
-        this.scheduleStart,
-        this.scheduleEnd});
-
-  Schedule.fromJson(Map<String, dynamic> json) {
-    scheduleId = json['schedule_id'];
-    scheduleDate = json['schedule_date'];
-    scheduleStart = json['schedule_start'];
-    scheduleEnd = json['schedule_end'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['schedule_id'] = this.scheduleId;
-    data['schedule_date'] = this.scheduleDate;
-    data['schedule_start'] = this.scheduleStart;
-    data['schedule_end'] = this.scheduleEnd;
-    return data;
-  }
-}
-
-class SeatRows {
+class Data {
   String row;
   List<Seats> seats;
 
-  SeatRows({this.row, this.seats});
+  Data({this.row, this.seats});
 
-  SeatRows.fromJson(Map<String, dynamic> json) {
+  Data.fromJson(Map<String, dynamic> json) {
     row = json['row'];
     if (json['seats'] != null) {
       seats = new List<Seats>();
@@ -115,20 +54,18 @@ class SeatRows {
 }
 
 class Seats {
-  String seatId;
-  int seatType;
+  int seatId;
+  String seatType;
   int number;
-  bool seatStatus;
-  int price;
+  dynamic seatStatus;
 
-  Seats({this.seatId, this.seatType, this.number, this.seatStatus, this.price});
+  Seats({this.seatId, this.seatType, this.number, this.seatStatus});
 
   Seats.fromJson(Map<String, dynamic> json) {
     seatId = json['seat_id'];
     seatType = json['seat_type'];
     number = json['number'];
     seatStatus = json['seat_status'];
-    price = json['price'];
   }
 
   Map<String, dynamic> toJson() {
@@ -137,7 +74,6 @@ class Seats {
     data['seat_type'] = this.seatType;
     data['number'] = this.number;
     data['seat_status'] = this.seatStatus;
-    data['price'] = this.price;
     return data;
   }
 }
